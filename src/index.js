6aea03504c7ba1f2b7af7c4f5028df3c528e2e8a
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-04-06 16:18:31 +0700

    Added root importing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
+// Main app load
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from 'App';
+import registerServiceWorker from 'assets/js/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Global css imports
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// React router related imports
+import { BrowserRouter } from 'react-router-dom';
+
+// React redux related imports
+import { Provider } from 'react-redux';
+import store from 'store.js';
+
+window.jQuery = window.$ = require('jquery');
+window.Popper = require('popper.js');
+require('bootstrap');
+
+ReactDOM.render((
+	<Provider store={ store }>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
+	), document.getElementById('root')
+);
+
 registerServiceWorker();
